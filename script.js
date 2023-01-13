ed0966af7c8a993e2c93f09e5e586cf505882462
ExeReta <exequiel.retascuy@gmail.com>
2023-01-13 04:50:17 -0300

    Primer commit del trabajo

---
+++ script.js
@@ -0,0 +1,14 @@
+document.
+
+
+
+//buscador de contenido
+
+//declarando variables
+
+ctn_bars_search = document.getElementById ("ctn-bars-search");
+inputSearch = document.getElementById ("inputSearch");
+box_search = document.getElementById("boxSearch");
+
+//funcion para mostrar el buscador
+
